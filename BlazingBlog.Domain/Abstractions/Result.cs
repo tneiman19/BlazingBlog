d18a9ad55fcc25dac2d2150e94215b94df9fcd5c
Steve Neiman <steveadmin@cammebys.com>
2025-03-27 14:59:13 -0400

    Add a result object

---
+++ BlazingBlog.Domain/Abstractions/Result.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazingBlog.Domain.Abstractions
+{
+    public class Result
+    {
+		public bool Success { get; }
+
+		public bool Failure => !Success;
+
+		public string? Error { get;  }
+
+		protected Result(bool success, string? errorMessage = null)
+		{
+			Success = success;
+			Error = errorMessage;
+		}
+
+		public static Result Ok() => new(true);
+
+		public static Result Fail(string errorMessage) => new(false, errorMessage);
+	}
+}
