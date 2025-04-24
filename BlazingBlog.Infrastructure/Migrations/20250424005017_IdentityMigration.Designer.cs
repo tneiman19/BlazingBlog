64dad8d938e7b6cde69a3e2ccdb165fd130843db
Steve Neiman <steveadmin@cammebys.com>
2025-04-23 21:07:31 -0400

    Chanfe the DbContext & Adding Migrations

--- BlazingBlog.Infrastructure/Migrations/20250424005017_IdentityMigration.Designer.cs
+++ BlazingBlog.Infrastructure/Migrations/20250424005017_IdentityMigration.Designer.cs
@@ -4,6 +4,7 @@ using BlazingBlog.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazingBlog.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250424005017_IdentityMigration")]
+    partial class IdentityMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -56,7 +59,7 @@ namespace BlazingBlog.Infrastructure.Migrations
 
                     b.HasIndex("UserId");
 
-                    b.ToTable("Articles", (string)null);
+                    b.ToTable("Articles");
                 });
 
             modelBuilder.Entity("BlazingBlog.Infrastructure.Authentication.User", b =>
