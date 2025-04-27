4ecefa7f523916c2a81b2fbbbe89cbf9f3bd054f
Steve Neiman <steveadmin@cammebys.com>
2025-04-27 14:29:40 -0400

    Create authentication service for user registration and login

---
+++ BlazingBlog.Application/Authentication/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazingBlog.Application.Authentication
+{
+	public interface IAuthenticationService
+	{
+		Task<RegisterUserResponse> RegisterUserAsync(string username, string email, string password);
+		Task<bool> LoginUserAsync(string username, string password);
+
+	}
+}
