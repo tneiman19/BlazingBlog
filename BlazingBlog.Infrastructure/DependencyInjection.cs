f9dc263ce37b989fdf52d7d720b5d138f0bd58f0
Steve Neiman <steveadmin@cammebys.com>
2025-04-28 14:43:10 -0400

    Register Authentication Services

--- BlazingBlog.Infrastructure/DependencyInjection.cs
+++ BlazingBlog.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,10 @@
-﻿using BlazingBlog.Domain.Articles;
+﻿using BlazingBlog.Application.Authentication;
+using BlazingBlog.Domain.Articles;
+using BlazingBlog.Infrastructure.Authentication;
 using BlazingBlog.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,10 +19,35 @@ namespace BlazingBlog.Infrastructure
 			{
 				options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
 
+				AddAuthentication(services);
+
 			});
 			services.AddScoped<IArticleRepository, ArticleRepository>();
 
 			return services;
+		}
+
+		private static void AddAuthentication(IServiceCollection services)
+		{
+			services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+			services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
+
+			services.AddCascadingAuthenticationState();
+
+			services.AddAuthorization();
+			services.AddAuthentication(options =>
+			{
+				options.DefaultScheme = IdentityConstants.ApplicationScheme;
+				options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+			})
+				.AddIdentityCookies();
+
+			services.AddIdentityCore<User>()
+			   .AddEntityFrameworkStores<ApplicationDbContext>()
+			   .AddSignInManager()
+			   .AddDefaultTokenProviders();
+
 		}
 	}
 }
