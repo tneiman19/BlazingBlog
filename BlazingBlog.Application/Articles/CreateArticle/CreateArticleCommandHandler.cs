2d19ed34813d62e5a5361c3728db42a23bb25372
Steve Neiman <steveadmin@cammebys.com>
2025-05-12 15:49:25 -0400

    Create an Article - Fixed

--- BlazingBlog.Application/Articles/CreateArticle/CreateArticleCommandHandler.cs
+++ BlazingBlog.Application/Articles/CreateArticle/CreateArticleCommandHandler.cs
@@ -1,19 +1,42 @@
-﻿namespace BlazingBlog.Application.Articles.CreateArticle
+﻿using BlazingBlog.Application.Exceptions;
+using BlazingBlog.Application.Users;
+
+namespace BlazingBlog.Application.Articles.CreateArticle
 {
 	public class CreateArticleCommandHandler : ICommandHandler<CreateArticleCommand, ArticleResponse>
 	{
 		private readonly IArticleRepository _articleRepository;
+		private readonly IUserService _userService;
 
-		public CreateArticleCommandHandler(IArticleRepository articleRepository)
+		public CreateArticleCommandHandler(IArticleRepository articleRepository, IUserService userService)
 		{
 			_articleRepository = articleRepository;
+			_userService = userService;
 		}
 
 		public async Task<Result<ArticleResponse>> Handle(CreateArticleCommand request, CancellationToken cancellationToken)
 		{
-			var newArticle = request.Adapt<Article>();
-			var article = await _articleRepository.CreateArticleAsync(newArticle);
-			return article.Adapt<ArticleResponse>();
+			try
+			{
+
+				var newArticle = request.Adapt<Article>();
+				newArticle.UserId = await _userService.GetCurrentUserIdAsync();
+				if (!await _userService.CurrentUserCanCreateArticlesAsync())
+				{
+					return FailingResult();
+				}
+				var article = await _articleRepository.CreateArticleAsync(newArticle);
+				return article.Adapt<ArticleResponse>();
+			}
+			catch (UserNotAuthorizedException)
+			{
+				return FailingResult();
+			}
+		}
+
+		private Result<ArticleResponse> FailingResult()
+		{
+			return Result.Fail<ArticleResponse>("You're not allowed to create articles. Sorry!  :/");
 		}
 	}
 }
